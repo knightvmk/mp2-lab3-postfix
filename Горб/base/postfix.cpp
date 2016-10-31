117c4af2c259406daa8c69812c10499023be39bc
Nikolay Gorb <knight.101@yandex.ua>
2016-10-31 17:17:59 +0300

    postfix.cpp
    in the begininng

--- Горб/base/postfix.cpp
+++ Горб/base/postfix.cpp
@@ -1,9 +1,17 @@
 ﻿#include "postfix.h"
 #include "stack.h"
 
+
+
 string TPostfix::ToPostfix()
 {
-  postfix = string("ab+");
+	TStack<char> operation(10);
+	for (register int i = 0; i < postfix.length; i++)
+	{
+		if (operation.IsEmpty() || operation.Top() == '(') 
+			operation.Push(postfix[i]);
+		//
+	}
   return postfix;
 }
 
