6177cb26ba8eaaaa6f3e23af8362ae371e2a40aa
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-11-02 07:59:28 +0300

    Update test_tstack.cpp

--- Горб/base_test/test_tstack.cpp
+++ Горб/base_test/test_tstack.cpp
@@ -1,6 +1,26 @@
 #include "stack.h"
 #include <gtest.h>
 
+TEST(TStack, can_create_stack_with_positive_length)
+{
+	ASSERT_NO_THROW(TStack<int> a(5));
+}
+
+TEST(TStack, throw_create_stack_with_overmax_lenght)
+{
+	ASSERT_ANY_THROW(TStack<int> a(105));
+}
+
+TEST(TStack, throw_create_stack_with_negative_lenght)
+{
+	ASSERT_ANY_THROW(TStack<int> a(-4));
+}
+
+TEST(TStack, throw_create_stack_with_zero_lenght)
+{
+	ASSERT_ANY_THROW(TStack<int> a(0));
+}
+
 TEST(TStack, operation_pop_push_is_true)
 {
 	TStack<double> a(2);
@@ -14,7 +34,7 @@ TEST(TStack, can_check_isfull_stack)
 {
 	TStack<int> a(2);
 	a.Push(50);
-	int a = a.Pop();
+	int b = a.Pop();
 	EXPECT_EQ(false, a.IsFull());
 }
 
@@ -22,8 +42,8 @@ TEST(TStack, can_check_isempty_stack)
 {
 	TStack<int> a(2);
 	a.Push(3);
-	int a = a.Pop();
-	EXPECT_EQ(true, a.IsEmpty);
+	int b = a.Pop();
+	EXPECT_EQ(true, a.IsEmpty());
 }
 
 TEST(TStack, throw_cant_pop_from_empty_stack)
@@ -36,5 +56,6 @@ TEST(TStack, cant_operation_top_to_change_stack)
 {
 	TStack<char> a(2);
 	a.Push('K');
-	EXPECT_EQ(a.Top(), a.Pop());
+	char r = a.Top();
+	EXPECT_EQ(a.Top(), 'K');
 }
