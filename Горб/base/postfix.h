7e9cf4b9e9d9e924226d5386121cbf0b7fdcb216
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-11-11 20:39:59 +0300

    Update postfix.h

--- Горб/base/postfix.h
+++ Горб/base/postfix.h
@@ -1,25 +1,42 @@
-﻿#ifndef __POSTFIX_H__
+#ifndef __POSTFIX_H__
 #define __POSTFIX_H__
 
 #include <string>
+#include <iostream>
 #include "stack.h"
 
+
 using namespace std;
+
+const struct prior
+{
+	char op;
+	int pr;
+};
 
 class TPostfix
 {
 private:
   string infix;
   string postfix;
+  void EraseSpace();
+  int prior(char c);
 public:
+
   TPostfix();
   TPostfix(const string &enter);
-  int prior(char c);
   void SetInfix(const string &enter);
-  void EraseSpace();
   string GetInfix() { return infix; }
   string GetPostfix() { return postfix; }
   string ToPostfix();
+  void WatchLen()
+  {
+	  printf("\ninf = %i post = %i\n", infix.length(), postfix.length());
+  }
+  void WatchSize()
+  {
+	  printf("\ninf = %i post = %i\n", infix.size(), postfix.size());
+  }
   double Calculate(); // Ввод переменных, вычисление по постфиксной форме
 };
 
