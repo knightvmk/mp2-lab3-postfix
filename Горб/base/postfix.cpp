7919e2ea3744ea420fd5bed39cef736dddad3746
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-11-11 20:38:19 +0300

    Update postfix.cpp

--- Горб/base/postfix.cpp
+++ Горб/base/postfix.cpp
@@ -4,8 +4,8 @@
 
 TPostfix::TPostfix()
 {
-	infix = "";
-	postfix = "";
+	infix ="";
+	postfix ="";
 }
 
 TPostfix::TPostfix(const string &enter)
@@ -25,6 +25,8 @@ int TPostfix::prior(char c) //for search priority
 	case '*':
 	case '/':
 		return 3;
+	case '^':
+		return 4;
 	case '(':
 		return 1;
 
@@ -45,26 +47,26 @@ void TPostfix::EraseSpace() //for true and exellent view
 		if (infix[i] == ' ')
 			infix.erase(i--, 1);
 	}
+
 }
 
 string TPostfix::ToPostfix()
 {
-	char not;
-	string origin_infix = infix;
+	//char not;
 	TStack<char> op(12);
 		//clear tab, space symbols;
 		//do extern function for delete spaces // look up
 		//we have a clear infix string without spaces
 	int j = 0; //for access and work with postfix
-	postfix.resize(MAX_SIZE); //for normal work
-	for (register int i = 0; i < infix.length(); i++,j++)
+	postfix.resize(infix.length()); //for normal work
+	for (register int i = 0; i < infix.length(); i++)
 	{
-		if (infix[i] != '+' && infix[i] != '-' && infix[i] != '*' && infix[i] != '/' && infix[i] != '(' && infix[i]!=')')
+		if (infix[i] != '+' && infix[i] != '-' && infix[i] != '*' && infix[i] != '/' && infix[i] != '^' && infix[i] != '(' && infix[i]!=')')
 		{
 			postfix[j++] = infix[i]; //any letter to postfix
 
 		}
-		else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] == '/' || infix[i] == '(')
+		else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] == '^' || infix[i] == '/' || infix[i] == '(')
 		{
 			if (op.IsEmpty() || op.Top()== '(') //oldbug: (op.IsEmpty() || prior(op.Top()) < prior(infix[i]) || infix[i] == '(')
 				op.Push(infix[i]);
@@ -85,7 +87,8 @@ string TPostfix::ToPostfix()
 			{
 				postfix[j++] = op.Pop();
 			}
-			not= op.Pop(); //delete "trash"
+			op.Pop();
+			//not=op.Pop(); //delete "trash"
 			//if (op.Top() == '(') not= op.Pop();
 		}
 		// if (infix[i] == '(') // look up to "oldbug"
@@ -94,16 +97,69 @@ string TPostfix::ToPostfix()
 	}
 	while (!op.IsEmpty())
 		postfix[j++] = op.Pop();
-	infix = origin_infix;
 	/*for (register int i = 0; i<postfix.length(); i++) ??not actually
 	{
 		if (postfix[i] == ' ')
 			postfix.erase(i--, 1);
 	}*/
+	for (register int i = 0; i<postfix.length(); i++)
+	{
+		if (postfix[i] == ' ')
+			postfix.erase(i--, 1);
+	}
 	return postfix;
 }
 
 double TPostfix::Calculate()
 {
-  return 0;
+	TStack<double> calc(postfix.length());
+	double digit1, digit2,on;
+	for (register int i = 0; i < postfix.length(); i++)
+	{
+		if (postfix[i] != '+' && postfix[i] != '-' && postfix[i] != '*' && postfix[i] != '/' && postfix[i] != '^')
+		{
+			printf("\nEnter %c =",postfix[i]);
+			scanf_s("%lf", &on);
+			calc.Push(on);
+		}
+		switch (postfix[i])
+		{
+		case '+':
+		{
+			digit1 = calc.Pop();
+			digit2 = calc.Pop();
+			calc.Push(digit1 + digit2);
+			break;
+		}
+		case '-':
+		{
+			digit1 = calc.Pop();
+			digit2 = calc.Pop();
+			calc.Push(digit2 - digit1);
+			break;
+		}
+		case '*':
+		{
+			digit1 = calc.Pop();
+			digit2 = calc.Pop();
+			calc.Push(digit1 * digit2);
+			break;
+		}
+		case '/':
+		{
+			digit1 = calc.Pop();
+			digit2 = calc.Pop();
+			calc.Push(digit2 / digit1);
+			break;
+		}
+		case '^':
+		{
+			digit1 = calc.Pop();
+			digit2 = calc.Pop();
+			calc.Push(pow(digit2, digit1));
+			break;
+		}
+		}
+	}
+	return calc.Pop();
 }
