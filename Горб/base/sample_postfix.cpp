6507f14c8d6c5b93563049d8a4e8c9376a622b2e
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-11-11 20:39:35 +0300

    add "debug" comment

--- Горб/base/sample_postfix.cpp
+++ Горб/base/sample_postfix.cpp
@@ -17,9 +17,9 @@ int main()
   cout << "Ваш ввод: "<< expression << endl; 
   TPostfix postfix(expression); //set postfix
   cout << "Арифметическое выражение: " << postfix.GetInfix() << endl; //return with space expression
-  postfix.ToPostfix();
-  postfix.WatchLen();
-  postfix.WatchSize();
+  postfix.ToPostfix(); 
+  postfix.WatchLen(); //for debug
+  postfix.WatchSize(); /for debug
   cout << "Постфиксная форма: " << postfix.GetPostfix() << endl; //inject nospace expression.infix to postfix
   res = postfix.Calculate();
   cout << "Результат: " << res << endl;
