d8a367e4704e6d1b68090b278df2b5ec0c55aee4
Nikolay Gorb <knightvmk@users.noreply.github.com>
2016-11-12 10:53:52 +0300

    Update sample_postfix.cpp

--- Горб/base/sample_postfix.cpp
+++ Горб/base/sample_postfix.cpp
@@ -1,24 +1,38 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include "postfix.h"
 
 using namespace std;
 
+void ShowRule()
+{
+	printf("\nПравило пользования программой:\n");
+	printf("  1) В качестве переменных необходимо вводить слова без содержания цифр,\n");
+	printf("  2) В качестве констант необходимо вводить только цифры, представляющие число,\n");
+	printf("  3) После перевода в постфиксную форму небходимо ввести значения переменных.\n");
+}
+
 int main()
 {
   string expression;
-  TPostfix postfix;
+  
   double res;
 
   setlocale(LC_ALL, "Russian");
-  //cout << "Введите арифметическое выражение: ";
+  ShowRule();
+  cout << "Введите арифметическое выражение: ";
+  getline(cin, expression); //true enter 
   //cin >> expression;
-  //cout << expression << endl;
-  cout << "Арифметическое выражение: " << postfix.GetInfix() << endl;
+  cout << "Ваш ввод: "<< expression << endl; 
+  TPostfix postfix(expression); //set postfix
+  cout << "Арифметическое выражение: " << postfix.GetInfix() << endl; //return with space expression
   postfix.ToPostfix();
-  cout << "Постфиксная форма: " << postfix.GetPostfix() << endl;
+  postfix.WatchLen(); //for debug
+  postfix.WatchSize(); //for debug
+  cout << "Постфиксная форма: " << postfix.GetPostfix() << endl; //inject nospace expression.infix to postfix
   res = postfix.Calculate();
-  cout << res << endl;
-
+  cout << "Результат: " << res << endl;
+  system("pause");
   return 0;
 }
+
